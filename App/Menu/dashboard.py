6343873ea15d8a0428a1fe25f0aabaec9f13e10a
harez13 <haerezaputra6@gmail.com>
2025-06-10 16:44:48 +0700

    changed 1

--- App/Menu/dashboard.py
+++ App/Menu/dashboard.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from streamlit_gsheets import GSheetsConnection
+# from streamlit_gsheets import GSheetsConnection
 
-conn = st.connection('gsheets', type=GSheetsConnection)
+# conn = st.connection('gsheets', type=GSheetsConnection)
 
-df=conn.read()
+# df=conn.read()
 
-st.write(df)
+# st.dataframe(df)
 
