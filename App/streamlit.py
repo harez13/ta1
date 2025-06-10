113225c8b609bb66949d927445aecd37c88df848
harez13 <haerezaputra6@gmail.com>
2025-06-11 01:04:34 +0700

    changed 1

--- App/streamlit.py
+++ App/streamlit.py
@@ -29,7 +29,7 @@ st.markdown(f"""
         }}
 
         /* Overlay hitam semi-transparan */
-        [data-testid="stAppViewContainer"] > .main::before {{
+        [data-testid="stAppViewContainer"] > .main: {{
             content: "";
             position: absolute;
             top: 0;
@@ -39,19 +39,6 @@ st.markdown(f"""
             background: rgba(0, 0, 0, 0.4); /* ubah angka ini untuk lebih terang/gelap */
             z-index: 0;
         }}
-
-        /* Overlay hitam semi-transparan */
-        [data-testid="stAppViewContainer"] > .main::after {{
-            content: "";
-            position: absolute;
-            top: 0;
-            right: 0;
-            bottom: 0;
-            left: 0;
-            background: rgba(0, 0, 0, 0.4); /* ubah angka ini untuk lebih terang/gelap */
-            z-index: 0;
-        }}
-
         /* Konten tetap tampil di atas overlay */
         [data-testid="stAppViewContainer"] > .main > div {{
             position: relative;
