112b6ce5441761f305126f5e1be50c69ae58d35c
harez13 <haerezaputra6@gmail.com>
2025-06-10 15:09:50 +0700

    initial commit

--- App/Menu/dashboard.py
+++ App/Menu/dashboard.py
@@ -3,13 +3,6 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Load data
-uploaded_file = st.file_uploader(
-    "Choose your database", accept_multiple_files=False)
-if uploaded_file is not None:
-    file_name = uploaded_file
-else:
-    file_name = "clean_data.csv"
 
 
 
