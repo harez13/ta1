96a848e5b6c966fc927e0e72b4f34cf7d71a712c
harez13 <haerezaputra6@gmail.com>
2025-06-10 14:58:00 +0700

    changed data2

--- App/Menu/dashboard.py
+++ App/Menu/dashboard.py
@@ -3,23 +3,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Load data
-df = pd.read_csv("Menu/clean_data.csv")
-df['tanggal'] = pd.to_datetime(df['tanggal'], dayfirst=True, errors='coerce')
-df['tahun'] = df['tanggal'].dt.year
-df['bulan'] = df['tanggal'].dt.month
-
-# Judul utama
-st.title("📊 Dashboard Kualitas Udara Jakarta 2024")
-
-# Sidebar navigasi
-option = st.sidebar.selectbox(
-    "Pilih Visualisasi:",
-    (
-        "1. Korelasi Antar Parameter Pencemar",
-        "2. Rata-Rata Bulanan Parameter Pencemar",
-        "3. Distribusi Kategori Kualitas Udara per Stasiun",
-        "4. Frekuensi Parameter Pencemar Kritis per Stasiun",
-        "5. Tren Harian PM2.5 per Stasiun"
-    )
-)
+uploaded_file = st.file_uploader(
+    "Choose your database", accept_multiple_files=False)
+if uploaded_file is not None:
+    file_name = uploaded_file
+else:
+    file_name = "clean_data.csv"
