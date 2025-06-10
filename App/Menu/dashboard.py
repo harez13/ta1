e7b3040be6d76a1de2099e83aca2b36ee65a4085
harez13 <haerezaputra6@gmail.com>
2025-06-10 16:47:52 +0700

    changed 1

--- App/Menu/dashboard.py
+++ App/Menu/dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-# from streamlit_gsheets import GSheetsConnection
+from streamlit_gsheets import GSheetsConnection
 
 # conn = st.connection('gsheets', type=GSheetsConnection)
 
