1a1e4769007624d61f1fbf1eea0275e1f847b50c
harez13 <haerezaputra6@gmail.com>
2025-06-10 14:34:08 +0700

    6

--- App/streamlit.py
+++ App/streamlit.py
@@ -10,46 +10,46 @@ import base64
 
 st.title('Test')
 
-#-- Page Setup --
+# -- Page Setup --
 
-# tentang_saya = st.Page(
-#     'Menu/tentang_saya.py',
-#     title = 'Tentang Saya',
-# )
+tentang_saya = st.Page(
+    'Menu/tentang_saya.py',
+    title = 'Tentang Saya',
+)
 
-# project1_page = st.Page(
-#     'Menu/main_project.py',
-#     title = 'Project',
-# )
+project1_page = st.Page(
+    'Menu/main_project.py',
+    title = 'Project',
+)
 
-# project2_page = st.Page(
-#     'Menu/dashboard.py',
-#     title = 'Dashboard',
-# )
+project2_page = st.Page(
+    'Menu/dashboard.py',
+    title = 'Dashboard',
+)
 
 #-- Navigation Setup [Without Sections] --
 # pg = st.navigation(pages = [tentang_saya, project1_page, project2_page])
 
-# #-- Navigation Setup With Sections
-# pg = st.navigation(
-#     {
-#         'Info' : [tentang_saya],
-#         'Projects' : [project1_page, project2_page]
-#     }
-# )
+#-- Navigation Setup With Sections
+pg = st.navigation(
+    {
+        'Info' : [tentang_saya],
+        'Projects' : [project1_page, project2_page]
+    }
+)
 
 
 
-pages = {
-    'Info' : [
-        st.Page('Menu/tentang_saya.py', title='Tentang Saya')
-    ],
-    'Projects' : [
-        st.Page('Menu/dashboard.py', title='Dashboard')
-    ]
-}
+# pages = {
+#     'Info' : [
+#         st.Page('Menu/tentang_saya.py', title='Tentang Saya')
+#     ],
+#     'Projects' : [
+#         st.Page('Menu/dashboard.py', title='Dashboard')
+#     ]
+# }
 
-pg = st.navigation(pages)
+# pg = st.navigation(pages)
 
 #-- Run Navigation
 pg.run()
