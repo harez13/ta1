f4509762e193e190177ee458e11b79d206a471ad
harez13 <haerezaputra6@gmail.com>
2025-06-11 00:18:51 +0700

    changed 1

--- App/streamlit.py
+++ App/streamlit.py
@@ -1,5 +1,30 @@
 # Library
 import streamlit as st
+import base64
+
+# # Fungsi untuk mendapatkan string base64 dari file gambar
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+# # Fungsi untuk mengatur gambar lokal sebagai latar belakang halaman
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    opacity : 0.8;
+    }
+    </style>
+    ''' % bin_str
+
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# # Menggunakan fungsi di atas
+set_png_as_page_bg('Images/tom-barrett--bSucp2nUdQ-unsplash (1).jpg')
 
 # -- Page Setup --
 
