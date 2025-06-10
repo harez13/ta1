6c6cb8819b364aebe873843ded4def94506dd227
harez13 <haerezaputra6@gmail.com>
2025-06-11 00:48:33 +0700

    changed 1

--- App/Menu/homepage.py
+++ App/Menu/homepage.py
@@ -303,28 +303,3 @@ Dalam studi lain, uji kecocokan diterapkan pada masker bedah untuk menguji efekt
 
 Walaupun kedua pengujian tersebut mengungkap bahwa masker bedah secara signifikan kurang efisien dibandingkan masker respirator (dengan peringkat N90-N100), masker bedah membantu mengurangi paparan polusi partikulat halus dengan biaya yang sangat rendah.
 """)
-
-
-# Fungsi untuk konversi gambar lokal ke base64
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-# Ganti dengan nama file gambar lokal kamu
-sidebar_bg = get_base64_of_bin_file("Images/tom-barrett--bSucp2nUdQ-unsplash (1).jpg")
-
-# Sisipkan CSS untuk sidebar
-st.markdown(
-    f"""
-    <style>
-        [data-testid="stSidebar"] {{
-            background-image: url("data:image/jpg;base64,{sidebar_bg}");
-            background-size: cover;
-            background-position: center;
-            opacity: 0.8;
-        }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
