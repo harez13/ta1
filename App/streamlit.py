6d79eee79b77d03c27adf178c7641d8f15c4595a
harez13 <haerezaputra6@gmail.com>
2025-06-11 00:40:59 +0700

    changed 1

--- App/streamlit.py
+++ App/streamlit.py
@@ -2,6 +2,9 @@
 import streamlit as st
 import base64
 
+st.set_page_config(layout="wide")
+
+
 # -- Page Setup --
 
 tentang_saya = st.Page(
@@ -56,3 +59,27 @@ pg = st.navigation(
 
 #-- Run Navigation
 pg.run()
+
+# Fungsi untuk konversi gambar lokal ke base64
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+# Ganti dengan nama file gambar lokal kamu
+sidebar_bg = get_base64_of_bin_file("Images/tom-barrett--bSucp2nUdQ-unsplash (1).jpg")
+
+# Sisipkan CSS untuk sidebar
+st.markdown(
+    f"""
+    <style>
+        [data-testid="stSidebar"] {{
+            background-image: url("data:image/jpg;base64,{sidebar_bg}");
+            background-size: cover;
+            background-position: center;
+            opacity: 0.8;
+        }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
