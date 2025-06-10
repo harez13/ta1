f52daf839be77cc121fe172495ef9d2251406784
harez13 <haerezaputra6@gmail.com>
2025-06-11 00:40:15 +0700

    changed 1

--- App/Menu/homepage.py
+++ App/Menu/homepage.py
@@ -312,7 +312,7 @@ def get_base64_of_bin_file(bin_file):
     return base64.b64encode(data).decode()
 
 # Ganti dengan nama file gambar lokal kamu
-sidebar_bg = get_base64_of_bin_file("sidebar_bg.jpg")
+sidebar_bg = get_base64_of_bin_file("Images/tom-barrett--bSucp2nUdQ-unsplash (1).jpg")
 
 # Sisipkan CSS untuk sidebar
 st.markdown(
