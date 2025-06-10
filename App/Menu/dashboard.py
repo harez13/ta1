096f3653cf4a9defbc9f3dcd9accbdfffc3a1dc7
harez13 <haerezaputra6@gmail.com>
2025-06-10 16:34:24 +0700

    changed 1

--- App/Menu/dashboard.py
+++ App/Menu/dashboard.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import plotly.express as px
 from streamlit_gsheets import GSheetsConnection
 
 conn = st.connection('gsheets', type=GSheetsConnection)
